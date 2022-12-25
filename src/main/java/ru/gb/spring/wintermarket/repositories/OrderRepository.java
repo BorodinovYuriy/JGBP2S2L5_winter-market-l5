a4200659345866cf00ff32d8d853aba6f3356883
aditone <aditone2378@gmail.com>
2022-12-25 23:47:01 +0300

    Рабочая версия распила - отпилена корзина, всё работает

--- src/main/java/ru/gb/spring/wintermarket/repositories/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.spring.wintermarket.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.gb.spring.wintermarket.entity.Order;
-@Repository
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
